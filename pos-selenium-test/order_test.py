c83d9757d74d45ab422977fc7b9a48e260e21bb9
Fahrizal <m.fahrizalalipradana@gmail.com>
2025-07-02 10:50:33 +0700

    update order_test.py

--- pos-selenium-test/order_test.py
+++ pos-selenium-test/order_test.py
@@ -1,11 +1,13 @@
 import random
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import datetime
 import time
 
 # ---------------------------------------- Initialize ---------------------------------------- 
 url = "http://localhost:5101/auth/login"
+url_order = "http://localhost:5101/order/createorder"
 
 # User
 username = "Fahrizal Ali"
@@ -71,17 +73,48 @@ try:
     pay_button.click()
     time.sleep(3)
 
-    va_option = driver.find_element(By.CLASS_NAME, "collapsible-payment--multiple__body")
-    va_option.click()
+    iframe = driver.find_element(By.TAG_NAME, "iframe")
+    driver.switch_to.frame(iframe)
+
+    title = driver.find_element(By.XPATH, "//div[@class='collapsible-payment--multiple__title']")
+    title.click()
     time.sleep(2)
 
     va_bca = driver.find_element(By.CSS_SELECTOR, "a[href='#/bank-transfer/bca-va']")
     va_bca.click()
+    time.sleep(5)
+
+    copy_va = driver.find_element(By.XPATH, "//div[@class='float-right clickable copy']")
+    copy_va.click()
     time.sleep(2)
 
+    driver.execute_script("window.open('https://simulator.sandbox.midtrans.com/bca/va/index', '_blank');")
+    driver.switch_to.window(driver.window_handles[1])
+    time.sleep(2)
+
+    merchant_input = driver.find_element(By.ID, "inputMerchantId")
+    merchant_input.send_keys(Keys.CONTROL, 'v')
+    time.sleep(2)
+
+    inquire_button = driver.find_element(By.XPATH, "//input[@type='submit']")
+    inquire_button.click()
+    time.sleep(2)
+
+    pay_button = driver.find_element(By.XPATH, "//input[@type='submit']")
+    pay_button.click()
+    time.sleep(2)
+
+    driver.switch_to.window(driver.window_handles[0])
+    driver.switch_to.frame(iframe)
+    time.sleep(2)
+
+    check_status_btn = driver.find_element(By.XPATH, "//button[@class='btn full primary  btn-theme']")
+    check_status_btn.click()
+    time.sleep(20)
 
 except Exception as e:
     print(f"Error during order: {e}")
     driver.quit()
     exit()
-
+finally:
+    generate_log("Order", "Success" if driver.current_url == url_order else "Error")
